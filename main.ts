52490d9090fb332f80be10ddc719b577bede9e84
HelloRocket <justin@hellorocket.org>
2020-09-07 13:28:56 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(1)
+    Choice += -1
+    basic.showString("" + (text_list[Choice]))
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showArrow(ArrowNames.North)
+    let list: number[] = []
+    Choice = 0
+    Computer_choice = list[randint(0, 2)]
+    basic.showString("" + (text_list[Choice]))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
+    Choice += 1
+    basic.showString("" + (text_list[Choice]))
 })
+let Computer_choice = 0
+let Choice = 0
+let text_list: string[] = []
 basic.showLeds(`
-    . . . . .
-    . . . . #
-    . . . # .
+    # # # # #
+    . . # . .
+    . . # . .
     # . # . .
     . # . . .
     `)
-basic.showString("Hello Rocket")
-basic.showNumber(5)
+text_list = ["Rock", "Paper", "Scissors"]
+basic.showIcon(IconNames.SmallSquare)
+basic.showIcon(IconNames.Scissors)
+basic.showIcon(IconNames.Square)
+basic.clearScreen()
