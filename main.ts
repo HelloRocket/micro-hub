001fc0d33afa08b642fefd5ee6d5eed1520d91db
HelloRocket <justin@hellorocket.org>
2020-09-07 14:01:48 +0900

    Changed to RSP

--- main.ts
+++ main.ts
@@ -3,16 +3,39 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("" + (text_list[Choice]))
 })
 input.onButtonPressed(Button.AB, function () {
-    let list: number[] = []
-    Choice = 0
-    Computer_choice = list[randint(0, 2)]
-    basic.showString("" + (text_list[Choice]))
+    Player_choice = text_list[Choice]
+    if (Computer_choice == "Rock" && Player_choice == "Scissors") {
+        Win = "Computer Wins"
+    } else if (Computer_choice == "Scissors " && Player_choice == "Paper") {
+        Win = "Computer Wins"
+    } else if (Computer_choice == "Paper" && Player_choice == "Rock") {
+        Win = "Computer Wins"
+    } else if (Computer_choice == "Rock" && Player_choice == "Paper") {
+        Win = "You Win!"
+    } else if (Computer_choice == "Paper" && Player_choice == "Scissors ") {
+        Win = "You Win!"
+    } else if (Computer_choice == "Rock" && Player_choice == "Paper") {
+        Win = "You Win!"
+    } else {
+        Win = "Tie"
+    }
 })
 input.onButtonPressed(Button.B, function () {
     Choice += 1
     basic.showString("" + (text_list[Choice]))
 })
-let Computer_choice = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Scissors)
+    basic.showIcon(IconNames.Square)
+    basic.clearScreen()
+    Choice = 0
+    Computer_choice = text_list[randint(0, 2)]
+    basic.showString("" + (text_list[Choice]))
+})
+let Win = ""
+let Computer_choice = ""
+let Player_choice = ""
 let Choice = 0
 let text_list: string[] = []
 basic.showLeds(`
@@ -22,8 +45,4 @@ basic.showLeds(`
     # . # . .
     . # . . .
     `)
-text_list = ["Rock", "Paper", "Scissors"]
-basic.showIcon(IconNames.SmallSquare)
-basic.showIcon(IconNames.Scissors)
-basic.showIcon(IconNames.Square)
-basic.clearScreen()
+text_list = ["Rock", "Scissors", "Paper"]
